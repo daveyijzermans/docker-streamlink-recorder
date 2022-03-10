12b6ac6e1bc2e47878c4869c720ebb3abad0a7c2
Davey IJzermans <47227341+daveyijzermans@users.noreply.github.com>
2022-03-10 21:28:44 +0100

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -4,6 +4,6 @@
 
 while [ true ]; do
 	Date=$(date +%Y%m%d-%H%M%S)
-	streamlink $streamOptions $streamLink $streamQuality -o /home/download/$streamName"-$Date".mkv
+	streamlink $streamOptions $streamLink $streamQuality -o "/home/download/{time} - {title}.mkv"
 	sleep 60s
 done
